685b4ef49fdcb5cbc6b2a750ca1f9e4047591721
Phil Vinson <rare72@users.noreply.github.com>
2014-05-03 09:53:41 -0400

    Update nginx5_install.sh
    Added standardization to the puppet module installer script

--- files/nginx5_install.sh
+++ files/nginx5_install.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+##
+# Make the Needed Directories
+##
 mkdir -m 777 -p /home/data/modules
 cd /home/data/modules
+##
+# Setup the Git Repo...
+##
 git init
 git remote add origin https://github.com/rare72/nginx_5.git
-git remote add origin git@github.com:rare72/nginx_5.git
 git pull origin master
+##
+# Apply permission to the repository file (Who knows how you will apply these files to the system)
+##
 chmod 777 -Rv /home/data/modules
+##
+# Run the Puppet created module
+##
 /usr/local/bin/puppet apply --verbose site.pp
