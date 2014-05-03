bc8ad04bc4baa8c3df652bc507a11f772a9fa137
Phil Vinson <rare72@users.noreply.github.com>
2014-05-03 14:22:32 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,7 @@
 include ntp
-$timestamp = generate('/bin/date +%Y%m%d%H%M')
-file {"/tmp/pappy_nginx5_${timestamp}":
- content => "Hello, The NGINX_5 module has completed.\n \n PV\n",
+$timestamp = generate('/bin/date" , '+%Y%m%d.%H%M')
+file {"/tmp/pappy_nginx5_${timestamp}.txt":
+ content => "Hello, The NGINX_5 module was executed.\n \n PV\n",
  ensure => 'file',
  }
 
