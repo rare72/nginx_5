8d5fb9fd3575e65d20acf48497e0a792d42642bf
Phil Vinson <rare72@users.noreply.github.com>
2014-05-05 13:51:44 -0400

    Update nginx5_cleanup.sh
    Added the ; to the end of each line. This way the script can be easily cut and paste on the Command Line.

--- files/nginx5_cleanup.sh
+++ files/nginx5_cleanup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-rm -rv /home/data/
-rm /etc/nginx/nginx.conf
-rm /etc/nginx/conf.d/puptest.conf
-rm /etc/nginx/sites-available/default
-rm /usr/share/nginx/www/index.html
-service nginx stop
+rm -rv /home/data/;
+rm /etc/nginx/nginx.conf;
+rm /etc/nginx/conf.d/puptest.conf;
+rm /etc/nginx/sites-available/default;
+rm /usr/share/nginx/www/index.html;
+service nginx stop;
 apt-get remove -y nginx
