102ec436e715db391cd8ef83c54d853a01a52eca
Phil Vinson <rare72@users.noreply.github.com>
2014-05-05 13:52:37 -0400

    Update nginx5_cleanup.sh

--- files/nginx5_cleanup.sh
+++ files/nginx5_cleanup.sh
@@ -7,3 +7,6 @@ rm /etc/nginx/sites-available/default;
 rm /usr/share/nginx/www/index.html;
 service nginx stop;
 apt-get remove -y nginx
+
+
+# rm -rv /home/data/;rm /etc/nginx/nginx.conf;rm /etc/nginx/conf.d/puptest.conf;rm /etc/nginx/sites-available/default;rm /usr/share/nginx/www/index.html;service nginx stop;apt-get remove -y nginx
